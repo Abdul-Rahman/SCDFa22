abb5fd3fa76991b39383f0272689298cbacb482c
Abdul-Rahman <armahmood786@yahoo.com>
2022-09-12 01:27:47 +0500

    Add files via upload

---
+++ eclipse-workspace/w02ex03-exceptions/src/StackTraceEx.java
@@ -0,0 +1,18 @@
+//program to print the exception information using printStackTrace() method
+
+public class StackTraceEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+	      int b=0;
+	        try{
+	          System.out.println(a/b);
+	        }
+	      catch(ArithmeticException e){
+	        e.printStackTrace();
+	      }
+	    }
+	}
+
+
